9850d9ed94a8a2fdc7665989cbbd941b5f13d95c
mat <mathewharvey@gmail.com>
2021-02-06 10:58:07 +0800

    /

--- server.js
+++ server.js
@@ -41,3 +41,11 @@ MongoClient.connect(
     setInterval(refresh, 10000);
   }
 );
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
